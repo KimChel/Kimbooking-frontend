5f58e3534a3a16d92a4cf692902447053638364b
parakakakias <kimonheliotis@yahoo.gr>
2022-06-15 12:16:05 +0300

    register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,14 @@
 import axios from 'axios'
+import { set } from 'date-fns'
 import React, { useContext, useState } from 'react'
+import { UNSAFE_NavigationContext, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
 import "./register.css"
 
 const Register = () => {
+
+    const [error, setError] = useState([false])
+
 
     const [credentials, setCredentials] = useState({
         username: "",
@@ -11,43 +16,60 @@ const Register = () => {
         password: "",
     })
 
+    const {username, password, email} = credentials
 
-    const {loading, error, dispatch } = useContext(AuthContext)
 
-    const handleChange = e => {
-        const { username, email, password } = e.target
+    const navigate = useNavigate()
+
+
+    const handleChange = name => event => {
+
         setCredentials({
             ...credentials,
-            [username]:username,
-            [email]:"email",
-            [password]:"password"
+            error: false,
+            [name]: event.target.value
         })
-        
+
     }
 
 
-    const register = () => {
-        const { username, email, password } = credentials
-        if (username && email && password) {
-            axios.post(`/auth/register`, credentials)
-                .then(res => console.log(res))
+    const register = async () => {
+
+        try {
+            await axios.post(`${REACT_APP_API_URL}/api/auth/register`, credentials)
+            navigate("/")
+
+        } catch (err) {
+            setCredentials({
+                ...credentials,
+                error: err.response.data.msg,
+            })
         }
-        else {
-            alert("invalid input")
-        }
+    }
+
+    const handeClick = (e) => {
+        e.preventDefault()
+        register({ username, email, password})
+        setCredentials({
+            ...credentials,
+            username: '',
+            email: '',
+            password:''
+        })
+
     }
 
 
     return (
         <div className="register">
 
-        <div className="registerContainer">
-            <input type="text" placeholder="username" id="username" username = {credentials.username} onChange={handleChange} className="registerInput" />
-            <input type="text" placeholder="email" id="email" email = {credentials.email} onChange={handleChange} className="registerInput" />
-            <input type="password" placeholder="password" id="password" password = {credentials.password} onChange={handleChange} className="registerInput" />
-            <button onClick={register} className="registerButton">Register</button>
-            {error && <span>{error.message}</span>}
-        </div>
+            <div className="registerContainer">
+                <input type="text" placeholder="username" id="username" value={username} onChange={handleChange('username')} className="registerInput" />
+                <input type="text" placeholder="email" id="email" value={email} onChange={handleChange('email')} className="registerInput" />
+                <input type="password" placeholder="password" id="password" value={password} onChange={handleChange('password')} className="registerInput" />
+                <button onClick={register} className="registerButton">Register</button>
+                {error && <span>{error.message}</span>}
+            </div>
         </div>
     )
 }
